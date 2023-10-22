9b9c02d852ea0cfb99843b67aa32db3b2d0bb159
Frank712 <franksuriel_7@hotmail.com>
2023-10-21 23:06:11 -0500

    ADD controller

---
+++ src/presentation/github/controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export class GithubController {
+  constructor() {}
+
+  webhookHandler = (req: Request, res: Response) => {
+    const githubEvent = req.header("x-github-event") ?? "unknow";
+    const signature = req.header("x-hub-signature-256") ?? "unknow";
+    const payload = req.body;
+    console.log({ githubEvent, signature });
+    res.status(202).send("accepted!");
+  };
+}
